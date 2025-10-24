b705dceade1ae8afe73c0902ceeabe4fb01cff30
bacemkarray <bkarray1@gmail.com>
2025-10-24 13:17:57 -0400

    populated user_db with helper functions.

--- user_db.py
+++ user_db.py
@@ -1,11 +1,36 @@
 import json
-import os
 
-USER_DB_PATH = "users.json"
 
 def load_users():
-    if os.path.exists(USER_DB_PATH):
-        with open(USER_DB_PATH, "r") as f:
-            return json.load(f)
-    else:
-        return {"users": {}}
+    with open("data/users.json", "r") as f:
+        return json.load(f)
+        
+
+def save_users(data):
+    with open("data/users.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def register_user(username, password):
+    data = load_users()
+    # Can't register multiple users with same username
+    for user in data:
+        if user["username"] == username:
+            return False
+
+    data.append({
+        "username": username, 
+        "password": password
+        })
+    save_users(data)    
+    return True
+
+
+def check_login(username, password):
+    data = load_users()
+    for user in data:
+        if user["username"] == username and user["password"] == password:
+            return True
+    return False
+
+register_user("Bacem", "1234")
