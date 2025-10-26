20d1a97677d05741a810a467903deaf23c6ee404
bacemkarray <bkarray1@gmail.com>
2025-10-26 13:04:36 -0400

    created egram_data file

---
+++ egram_data.py
@@ -0,0 +1,21 @@
+class EgramData:
+    def __init__(self):
+        self.time = []
+        self.values = []
+    
+    # Method for wiping all the current egram data stored
+    def clear(self):
+        self.time.clear()
+        self.values.clear()
+
+        
+
+egram = EgramData()
+
+egram.time.append(40)
+egram.values.append(40)
+print(egram.time)
+print(egram.values)
+egram.clear()
+print(egram.time)
+print(egram.values)
